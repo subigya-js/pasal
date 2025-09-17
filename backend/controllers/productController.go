c546a38ab8ea1a64acc2fe1e4186dd69e6f8300d
subigya-js <developer.subigya@gmail.com>
2025-09-17 22:55:52 +0545

    feat: implement CreateProduct function with validation and default values

--- backend/controllers/productController.go
+++ backend/controllers/productController.go
@@ -1,6 +1,57 @@
 package controllers
 
-func CreateProduct() {}
+import (
+	"backend/database"
+	"backend/model"
+	"context"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+func CreateProduct(c *gin.Context) {
+	productCollection := database.GetCollection("products")
+	var product model.Product
+
+	if err := c.ShouldBindJSON(&product); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	// Set default values
+	product.ID = primitive.NewObjectID()
+	product.CreatedAt = time.Now()
+	product.UpdatedAt = time.Now()
+	product.IsActive = true
+
+	if product.Stock < 0 {
+		product.Stock = 0
+	}
+
+	res, err := productCollection.InsertOne(ctx, product)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"error": "Failed to craete product.",
+		})
+		log.Println("Failed to create product:", err)
+		return
+	}
+
+	c.JSON(http.StatusCreated, gin.H{
+		"status":     "success",
+		"message":    "Product created successfully.",
+		"product_id": res.InsertedID,
+		"product":    product,
+	})
+}
 
 func GetAllProducts() {}
 
