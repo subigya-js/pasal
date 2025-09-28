9c1f05675a0ade69fd0433302e78338eb52ff907
subigya-js <developer.subigya@gmail.com>
2025-09-29 00:56:04 +0545

    feat: define Order struct with relevant fields in model package

--- backend/model/order.go
+++ backend/model/order.go
@@ -1,5 +1,29 @@
 package model
 
+import (
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
 type Order struct {
-	
+	ID              primitive.ObjectID `json:"id,omitempty" bson:"_id,omitempty"`
+	OrderNumber     string             `json:"order_number" bson:"order_number"`
+	UserID          primitive.ObjectID `json:"user_id" bson:"user_id"`
+	Items           []OrderItem        `json:"items" bson:"items"`
+	TotalQuantity   int                `json:"total_quantity" bson:"total_quantity"`
+	TotalPrice      float64            `json:"total_price" bson:"total_price"`
+	ShippingAddress ShippingAddress    `json:"shipping_address" bson:"shipping_address"`
+	OrderStatus     string             `json:"order_status" bson:"order_status"`
+	PaymentStatus   string             `json:"payment_status" bson:"payment_status"`
+	PaymentMode     string             `json:"payment_mode" bson:"payment_mode"`
+	Notes           string             `json:"notes" bson:"notes"`
+	CreatedAt       time.Time          `json:"created_at" bson:"created_at"`
+	UpdatedAt       time.Time          `json:"updated_at" bson:"updated_at"`
+}
+
+type OrderItem struct {
+}
+
+type ShippingAddress struct {
 }
