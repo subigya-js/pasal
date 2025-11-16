8b8a9d2bde4ebdfffae6aa39c9654bbdd225c180
subigya-js <developer.subigya@gmail.com>
2025-11-16 17:42:19 +0545

    feat: add sample items constant and implement product display in Home page

---
+++ frontend/constants/sample.js
@@ -0,0 +1,84 @@
+export const sampleItems = [
+  {
+    id: 1,
+    name: "Real Madrid Home 25/26",
+    description: "Season 25-26",
+    price: "Rs. 1199.00",
+    sku: "RM2526HOME",
+    category_id: "",
+    rating: 4.5,
+    stock: 10,
+    images: [
+      {
+        url: "https://dukaan.b-cdn.net/700x700/webp/media/58a48494-680e-4bcf-9759-812314ac26df.png",
+        alt: "Real Madrid Home 25/26",
+      },
+      {
+        url: "https://dukaan.b-cdn.net/700x700/webp/media/4a58dfa6-6969-4769-b91c-11d9d7bf427d.png",
+        alt: "Real Madrid Home 25/26",
+      },
+      {
+        url: "https://dukaan.b-cdn.net/700x700/webp/media/a98b5efc-c255-4c32-801c-298603a0e0b4.png",
+        alt: "Real Madrid Home 25/26",
+      },
+      {
+        url: "https://dukaan.b-cdn.net/700x700/webp/media/751a7173-6999-4e09-9bf7-fe05c93d18ef.png",
+        alt: "Real Madrid Home 25/26",
+      },
+      {
+        url: "https://dukaan.b-cdn.net/700x700/webp/media/440313d6-0224-441d-af7b-0e7a910ae702.png",
+        alt: "Real Madrid Home 25/26",
+      },
+      {
+        url: "https://dukaan.b-cdn.net/700x700/webp/media/0242820d-6016-4b8b-ae6e-bc3fbd14feb5.png",
+        alt: "Real Madrid Home 25/26",
+      },
+      {
+        url: "https://dukaan.b-cdn.net/700x700/webp/media/00a43591-0c0a-412d-805e-18c0633bf517.png",
+        alt: "Real Madrid Home 25/26",
+      },
+    ],
+    is_active: true,
+  },
+  {
+    id: 2,
+    name: "Barcelona Home 25/26",
+    description: "Season 25-26",
+    price: "Rs. 999.00",
+    sku: "BAR2526HOME",
+    category_id: "",
+    rating: 3,
+    stock: 8,
+    images: [
+      {
+        url: "https://dukaan.b-cdn.net/700x700/webp/media/d0ea6646-9893-4faf-9d8b-398b93c80cc1.png",
+        alt: "Barcelona Home 25/26",
+      },
+      {
+        url: "https://dukaan.b-cdn.net/700x700/webp/media/7e00c980-75e4-4e93-95b5-5c455dc2006c.png",
+        alt: "Barcelona Home 25/26",
+      },
+      {
+        url: "https://dukaan.b-cdn.net/700x700/webp/media/a9a3124f-5ca4-4d1c-a940-24ab445656c7.png",
+        alt: "Barcelona Home 25/26",
+      },
+      {
+        url: "https://dukaan.b-cdn.net/700x700/webp/media/9501adcc-4e25-4963-9002-346214797397.png",
+        alt: "Barcelona Home 25/26",
+      },
+      {
+        url: "https://dukaan.b-cdn.net/700x700/webp/media/f96c15b0-35d1-40a0-b27b-b41119d7a709.png",
+        alt: "Barcelona Home 25/26",
+      },
+      {
+        url: "https://dukaan.b-cdn.net/700x700/webp/media/8b22427d-d5d9-401b-9fc4-3592605effbd.png",
+        alt: "Barcelona Home 25/26",
+      },
+      {
+        url: "https://dukaan.b-cdn.net/700x700/webp/media/542946fe-c4e3-41fb-b34b-da0261af25ce.png",
+        alt: "Barcelona Home 25/26",
+      },
+    ],
+    is_active: true,
+  },
+];
--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,10 +1,38 @@
-import Navbar from '@/components/LandingPage/Navbar';
-import React from 'react';
+import { Button } from '@/components/ui/button';
+import { sampleItems } from '../../constants/sample';
 
 export default function Home() {
   return (
     <div className="font-sans">
-      <Navbar />
+      <div className="container mx-auto p-4">
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+          {sampleItems.map((item) => (
+            <div key={item.id} className="rounded-xl overflow-hidden transition-shadow py-2">
+              <div className='bg-gray-100 cursor-pointer p-2 rounded-lg hover:shadow-md duration-300 transition-shadow'>
+                <div className="relative overflow-hidden">
+                  <img
+                    src={item.images[0].url}
+                    alt={item.images[0].alt}
+                    className="w-full h-full object-cover rounded-xl"
+                  />
+                </div>
+                <div className="p-4 text-center">
+                  <h3 className="font-bold text-lg">{item.name}</h3>
+                  <p className="text-gray-600 text-sm">{item.description}</p>
+                </div>
+              </div>
+              <div className='flex flex-col gap-2 mt-2 px-2'>
+                <p className="font-bold text-indigo-600 text-center">{item.price}</p>
+                <div className='flex justify-between'>
+                  <Button variant="outline" className=" text-black w-full py-1 px-2 rounded cursor-pointer">
+                    Add to Cart
+                  </Button>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
