47787ae872865ee382c7db5dd91aefdae2b27dc6
subigya-js <developer.subigya@gmail.com>
2025-11-17 22:33:22 +0545

    feat: add product page and enhance sample items with type and sizes

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import { Button } from '@/components/ui/button';
 import { sampleItems } from '../../constants/sample';
 
@@ -8,27 +9,32 @@ export default function Home() {
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
           {sampleItems.map((item) => (
             <div key={item.id} className="rounded-xl overflow-hidden transition-shadow py-2">
-              <div className='bg-gray-100 cursor-pointer p-2 rounded-lg hover:shadow-md duration-300 transition-shadow'>
-                <div className="relative overflow-hidden">
-                  <img
-                    src={item.images[0].url}
-                    alt={item.images[0].alt}
-                    className="w-full h-full object-cover rounded-xl"
-                  />
+
+              <Link href={`/${item.id}`}>
+                <div className="bg-gray-100 cursor-pointer p-2 rounded-lg hover:shadow-md duration-300 transition-shadow">
+                  <div className="relative overflow-hidden">
+                    <img
+                      src={item.images[0].url}
+                      alt={item.images[0].alt}
+                      className="w-full h-full object-cover rounded-xl"
+                    />
+                  </div>
+                  <div className="p-4 text-center">
+                    <h3 className="font-bold text-lg">{item.name}</h3>
+                    <p className="text-gray-600 text-sm">{item.description}</p>
+                  </div>
                 </div>
-                <div className="p-4 text-center">
-                  <h3 className="font-bold text-lg">{item.name}</h3>
-                  <p className="text-gray-600 text-sm">{item.description}</p>
-                </div>
-              </div>
+              </Link>
+
               <div className='flex flex-col gap-2 mt-2 px-2'>
                 <p className="font-bold text-blue-500 text-center">{item.price}</p>
                 <div className='flex justify-between'>
-                  <Button variant="outline" className=" text-black w-full py-1 px-2 rounded cursor-pointer">
+                  <Button variant="outline" className="text-black w-full py-1 px-2 rounded cursor-pointer">
                     Add to Cart
                   </Button>
                 </div>
               </div>
+
             </div>
           ))}
         </div>
